873f8c70215ba69c31179fab0bd38f6ae421afc6
mohammadHosein <mohammadhoseindadgostr@gmail.com>
2025-07-19 22:28:00 +0330

    Added Initial Files Resome

---
+++ miniproject-s5/src/pages/index.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-purple-700">
+        Tailwind با موفقیت نصب شد! 🎉
+      </h1>
+    </div>
+  );
+}
