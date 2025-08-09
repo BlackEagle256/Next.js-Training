f645e0643348100d58d093de3f0cd3b43bb91ed5
mohammadHosein <mohammadhoseindadgostr@gmail.com>
2025-08-09 17:10:41 +0330

    Some Styles Changed

--- season7-state-managment/src/pages/index.js
+++ season7-state-managment/src/pages/index.js
@@ -4,20 +4,33 @@ export default function Home() {
   const { isLoggedIn, user, login, logout } = useAuth();
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Home Page</h1>
+    <div className='w-full h-screen flex flex-col justify-center items-center text-center pt-14'>
+      <div className='flex flex-col items-center justify-center'>
 
-      {isLoggedIn ? (
-        <>
-          <p>Welcome, {user.name}!</p>
-          <button onClick={logout}>Logout</button>
-        </>
-      ) : (
-        <>
-          <p>You are not logged in</p>
-          <button onClick={() => login("John Doe")}>Login</button>
-        </>
-      )}
+        <h1>Home Page</h1>
+
+        {isLoggedIn ? (
+          <>
+            <p>Welcome, {user.name}!</p>
+            <button
+              className='bg-red-500 px-6 py-4 rounded-xl my-4 text-white'
+              onClick={logout}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <p>You are not logged in</p>
+            <button
+              className='bg-blue-500 px-6 py-4 rounded-xl my-4 text-white'
+              onClick={() => login("John Doe")}
+            >
+              Login
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
